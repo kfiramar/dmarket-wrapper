4d534bc66ed96e322654479b132cfcb0d276bdd5
kfir <kfira2002@gmail.com>
2022-09-16 12:06:39 +0300

    added generic functions

--- targetCreator.py
+++ targetCreator.py
@@ -10,103 +10,42 @@ secret_key = "95bf02a93d0243ccfd234117c5005bf20767896b874ad226e37d4392c68629b387
 # change url to prod
 rootApiUrl = "https://api.dmarket.com"
 
+signature_prefix = "dmar ed25519 "
 
 
-
-
-
-
-
-
-
-
-
-
-def get_inventory():
-    response = requests.get(rootApiUrl + "/marketplace-api/v1/user-inventory?gameId=a8db&limit=1&currency=USD")
-    offers = json.loads(market_response.text)["objects"]
-    return offers[0]
-    market_response = requests.get(rootApiUrl + "/marketplace-api/v1/user-inventory?GameID=a8db&BasicFilters.InMarket=true&SortType=UserInventorySortTypeDefault&Presentation=InventoryPresentationSimple&Limit=5000") 
-    offers = json.loads(market_response.text)["objects"]
-    for offer in offers:
-        print (offer)
-    return offers
-
-def build_target_body_to_get_inventory(offer):
-    return {"targets": [
-        {"amount": 1, "gameId": offer["gameId"], "price": {"amount": "2", "currency": "USD"},
-         "attributes": {"gameId": offer["gameId"],
-                        "categoryPath": offer["extra"]["categoryPath"],
-                        "title": offer["title"],
-                        "name": offer["title"],
-                        "image": offer["image"],
-                        "ownerGets": {"amount": "1", "currency": "USD"}}}
-    ]}
-    
-    
-    
-    
-    
-def build_target_body_from_offer(offer):
-    return {"Items": [
-        {"amount": 1, "gameId": offer["gameId"], "price": {"amount": "2", "currency": "USD"},
-         "attributes": {"gameId": offer["gameId"],
-                        "categoryPath": offer["extra"]["categoryPath"],
-                        "title": offer["title"],
-                        "name": offer["title"],
-                        "image": offer["image"],
-                        "ownerGets": {"amount": "1", "currency": "USD"}}}
-    ]}
-    
-
-
-# nonce = str(round(datetime.now().timestamp()))
-# api_url_path = "/exchange/v1/target/create"
-# method = "POST"
-# offer_from_market = get_offer_from_market()
-# body = build_target_body_from_offer(offer_from_market)
-# string_to_sign = method + api_url_path + json.dumps(body) + nonce
-# signature_prefix = "dmar ed25519 "
-# encoded = string_to_sign.encode('utf-8')
-# secret_bytes = bytes.fromhex(secret_key)
-# signatsures_bytes = crypto_sign(encoded, bytes.fromhex(secret_key))
-# signature = signatsures_bytes[:64].hex()
-# headers = {
-#     "X-Api-Key": public_key,
-#     "X-Request-Sign": signature_prefix + signature,
-#     "X-Sign-Date": nonce
-# }
-
-# resp = requests.post(rootApiUrl + api_url_path, json=body, headers=headers)
-# print(resp.text)
-
-
-
-def inventory_request():
+def create_headers(api_url_path, body=''):
   nonce = str(round(datetime.now().timestamp()))
-  api_url_path = "/marketplace-api/v1/user-inventory?BasicFilters.InMarket=true&Limit=1"
   method = "GET"
-  #body = build_target_body_to_get_inventory()
-  #string_to_sign = method + api_url_path + json.dumps(body) + nonce
+  # string_to_sign = method + api_url_path + json.dumps(body) + nonce
   string_to_sign = method + api_url_path + nonce
-  signature_prefix = "dmar ed25519 "
   encoded = string_to_sign.encode('utf-8')
-  secret_bytes = bytes.fromhex(secret_key)
   signature_bytes = crypto_sign(encoded, bytes.fromhex(secret_key))
   signature = signature_bytes[:64].hex()
-  headers = {
+  return {
       "X-Api-Key": public_key,
       "X-Request-Sign": signature_prefix + signature,
       "X-Sign-Date": nonce
   }
+
+
+def inventory_request():
+  api_url_path = "/marketplace-api/v1/user-inventory?BasicFilters.InMarket=true&Limit=1"
+  headers = create_headers(api_url_path)
   # resp = requests.post(rootApiUrl + api_url_path, json=body, headers=headers)
   resp = requests.get(rootApiUrl + api_url_path, headers=headers)
-  json_resp = resp.json()
-  f = open("response.txt","w")
- # f.write((json_resp['Items'][0]['AssetID']))
-  for field in str(json_resp['Items'][0]['Attributes']):
-    f.write(field)
+  write_content('response.txt',resp.json())
   
+  
+def generic_request(api_url_path):
+  headers = create_headers(api_url_path)
+  resp = requests.get(rootApiUrl + api_url_path, headers=headers)
+  write_content('response.txt',resp.json())
+  
+
+def write_content(file_name,content):
+  f = open(file_name,"w")
+  f.write(str(content['Items']))
   
 if __name__ == '__main__':
-  inventory_request()
+  #inventory_request()
+  generic_request("/marketplace-api/v1/user-inventory?BasicFilters.InMarket=true&Limit=1")
